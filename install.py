ebfe763a9a2f3344d5d478a3031e6cbaef32701e
David Rotthoff <david.rotthoff@coxautoinc.com>
2017-12-18 16:15:57 -0600

    Updated post-commit hook

--- install.py
+++ install.py
@@ -12,20 +12,23 @@ git_hook = 'git-hook.py'
 
 git_dirs = {'filter_dir': 'filters', 'event_dir': 'hooks'}
 
-git_hooks = [{'git_event': 'post_commit', 'event_code': 'rcs-keywords-hook-post-commit.py'},
-             {'git_event': 'post_checkout', 'event_code': 'rcs-keywords-hook-post-checkout.py'},
-             {'git_event': 'post_merge', 'event_code': 'rcs-keywords-hook-post-merge.py'}]
+git_hooks = [{'event_name': 'post-commit', 'event_code': 'rcs-keywords-hook-post-commit.py'},
+             {'event_name': 'post-checkout', 'event_code': 'rcs-keywords-hook-post-checkout.py'},
+             {'event_name': 'post-merge', 'event_code': 'rcs-keywords-hook-post-merge.py'}]
 
-git_filters = ['rcs-keywords-filter-clean.py', 'rcs-keywords-filter-smudge.py']
+git_filters = [{'filter_type': 'clean', 'filter_name': 'rcs-keywords-filter-clean.py'},
+               {'filter_type': 'smudge', 'filter_name': 'rcs-keywords-filter-smudge.py'}]
 
-git_file_pattern = ['*.sql', '*.txt', '*.yml', '*.yaml', '*.xml', '*.jsn', '*.json',
-                    '*.pl', '*.sh', '*.md', '*.ora', '*.py' ]
+git_file_pattern = ['*.sql', '*.ora', '*.txt', '*.md', '*.yml', '*.yaml', '*.hosts', '*.xml',
+                    '*.jsn', '*.json', '*.pl', '*.py', '*.sh' ]
 
 import sys
 import os
 import time
 from shutil import copy2
-import errno
+import subprocess
+import re
+#import errno
 
 # Set the debugging flag
 summary_flag = bool(True)
@@ -80,13 +83,13 @@ def copyfile (srcfile, destfile):
 
     # If the file already exists, throw the appropriate exception
     if os.path.exists(destfile):
-        sys.stderr.write('  Destination file already exist\n')
+        sys.stderr.write('  Destination file already exist -- OVERWRITTING!!!!\n')
 #        raise OSError(errno.EEXIST, 'File %s already exists' % destfile)
     # Copy the source file to the destination file
-    else:
-        copy2(srcfile, destfile)
-        if summary_flag:
-            sys.stderr.write('  Copied file  %s to %s\n' % (srcfile, destfile))
+    # TODO:  Make sure it is overwritting as planned!!!!
+    copy2(srcfile, destfile)
+    if summary_flag:
+        sys.stderr.write('  Copied file  %s to %s\n' % (srcfile, destfile))
     return;
 
 
@@ -94,7 +97,7 @@ def registergitevent (eventdir, eventname, eventcode):
     if debug_flag:
         sys.stderr.write('  git event dir: %s\n' % eventdir)
         sys.stderr.write('  git event name: %s\n' % eventname)
-        sys.stderr.write('  event code: %s\n' % eventcode)
+        sys.stderr.write('  git event code: %s\n' % eventcode)
     event_code_dir = os.path.join(eventdir, '%s.d' % eventname)
     if debug_flag:
         sys.stderr.write('  git event code dir: %s\n' % event_code_dir)
@@ -112,11 +115,12 @@ def registergitevent (eventdir, eventname, eventcode):
 
 
 def installgitkeywords (git_dir, repo_dir):
-    target_dir = None
-
+#    target_dir = None
+#
     if debug_flag:
         sys.stderr.write('  Target git directory: %s\n' % git_dir)
         sys.stderr.write('  Repository directory: %s\n' % repo_dir)
+
     # Validate that the installation target has a .git directory
     if not validatedirexists(dirname = git_dir):
         sys.stderr.write('  Target git directory %s is not a git repository\n' % git_dir)
@@ -124,35 +128,71 @@ def installgitkeywords (git_dir, repo_dir):
         exit(1)
 
     # Create the core directories
-    createdir(dirname = os.path.join(git_dir, git_dirs['filter_dir']))
-    createdir(dirname = os.path.join(git_dir, git_dirs['event_dir']))
+    filter_dir = os.path.join(git_dir, git_dirs['filter_dir'])
+    createdir(dirname = filter_dir)
+    event_dir = os.path.join(git_dir, git_dirs['event_dir'])
+    createdir(dirname = event_dir)
 
-    # Copy the filter programs into place
-    for filter_name in git_filters:
-        copyfile(srcfile = os.path.join(program_path, filter_name), destfile = os.path.join(git_dir, git_dirs['filter_dir'], filter_name))
+    # Set up the filter programs for use
+    for filter_def in git_filters:
+        # Gather the filter name & type for each filter processed
+        filter_name = filter_def['filter_name']
+        filter_type = filter_def['filter_type']
+        if debug_flag:
+            sys.stderr.write('  Copying filter: %s\n' % filter_name)
+
+        # Copy the filter program to the target directory
+        copyfile(srcfile = os.path.join(program_path, filter_name), destfile = os.path.join(filter_dir, filter_name))
+        if debug_flag:
+            sys.stderr.write('  Registering filter: %s\n' % filter_name)
+
+        # Register the filter program to rcs-keywords filter
+        git_cmd = 'git config --local filter.rcs-keywords.%s "%s %s"' % (filter_type, os.path.join(filter_dir, filter_name), '%f')
+        if debug_flag:
+            sys.stderr.write('  git_cmd: %s\n' % git_cmd)
+        git_return = subprocess.Popen([git_cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        (git_stdout, git_stderr) = git_return.communicate()
+        if debug_flag:
+            sys.stderr.write('    git exit code: %s\n' % str(git_return.returncode))
+            sys.stderr.write('    stdout length: %s\n' % str(len(git_stdout)))
+            sys.stderr.write('    stderr length: %s\n' % str(len(git_stderr)))
 
 
-    # Need to add registering the filters
-    # Configure the filters for use
-    # echo "Configuring the filters for use"
-    # git config --local filter.rcs-keywords.clean "${clean_filter}"
-    git_cmd = 'git config --local filter.rcs-keywords.clean "%s %s"' % ('Clean.filter', '%f')
-    if debug_flag:
-        sys.stderr.write('  git_cmd: %s\n' % git_cmd)
-    # git config --local filter.rcs-keywords.smudge "${smudge_filter} %f"
-    git_cmd = 'git config --local filter.rcs-keywords.smudge "%s %s"' % ('Smudge.filter', '%f')
-    if debug_flag:
-        sys.stderr.write('  git_cmd: %s\n' % git_cmd)
-    # echo "rcs-keywords configuring for use"
+    # Register the defined file patterns to the filters in the attributes files
+    attribute_file = os.path.join(git_dir, 'info', 'attributes')
+    attribute_backup = os.path.join(git_dir, 'info', 'attributes~')
 
-    # Need to register the file patterns to the filters in the attributes files
+    # If the attribute file already exists, rename the file and re-create it 
+    if os.path.exists(attribute_file):
+        os.rename(attribute_file, attribute_backup)
+        keyword_regex = re.compile("rcs-keywords", re.IGNORECASE)
+        source = open(attribute_backup, "r")
+        destination = open(attribute_file, "w")
+        for line in source:
+            if not keyword_regex.search(line):
+                destination.write(line)
+        source.close()
+    else:
+        destination = open(attribute_file, "w")
 
+    # Write the appropriate file patterns for the filter usage
+    max_len = len(max(git_file_pattern, key=len))
+    sys.stderr.write('  max file pattern length: %d\n' % max_len)
+    for file_pattern in git_file_pattern:
+        destination.write('%s filter=rcs-keywords\n' % file_pattern.ljust(max_len))
+    destination.close() 
+             
     # Copy the git event manager
-    copyfile(srcfile = os.path.join(program_path, git_hook), destfile = os.path.join(git_dir, git_dirs['event_dir'], git_hook))
+    copyfile(srcfile = os.path.join(program_path, git_hook), destfile = os.path.join(event_dir, git_hook))
 
     # Register the git event hooks
-    for event_name in git_hooks:
-        registergitevent(eventdir = os.path.join(git_dir, git_dirs['event_dir']), eventname = event_name['git_event'], eventcode = event_name['event_code'])
+    for git_event in git_hooks:
+        # Gather the filter name & type for each filter processed
+        event_name = git_event['event_name']
+        event_code = git_event['event_code']
+        if debug_flag:
+            sys.stderr.write('  Registering event %s\n' % event_name)
+        registergitevent(eventdir = event_dir, eventname = event_name, eventcode = event_code)
     return;
 
 
@@ -179,10 +219,12 @@ if summary_flag:
 # Set the installation target
 if len(sys.argv) > 1:
     target_dir = sys.argv[1]
-    sys.stderr.write('  Target from parameter: %s\n' % target_dir)
+    if debug_flag:
+        sys.stderr.write('  Target from parameter: %s\n' % target_dir)
 else:
-    target_dir = '.'
-    sys.stderr.write('  Target default: %s\n' % target_dir)
+    target_dir = ''
+    if debug_flag:
+        sys.stderr.write('  Target default: %s\n' % target_dir)
 if summary_flag:
     sys.stderr.write('  Target directory: %s\n' % target_dir)
 
@@ -221,30 +263,6 @@ if timing_flag:
 
 installgitkeywords (git_dir = os.path.join(target_dir, '.git'), repo_dir = target_dir)
 
-## Validate that the installation target has a .git directory
-#if not validatedirexists(dirname = os.path.join(target_dir, '.git')):
-#    sys.stderr.write('Target installation directory %s is not a git repository\n' % target_dir)
-#    sys.stderr.write('Aborting installation!\n')
-#    exit(1)
-
-## Create the core directories
-#createdir(dirname = os.path.join(target_dir, git_dirs['filter_dir']))
-#createdir(dirname = os.path.join(target_dir, git_dirs['event_dir']))
-
-## Copy the filter programs into place
-#for filter_name in git_filters:
-#    copyfile(srcfile = os.path.join(program_path, filter_name), destfile = os.path.join(target_dir, git_dirs['filter_dir'], filter_name))
-
-
-## Need to add registering the filters
-## Need to register the file patterns to the filters in the attributes files
-
-## Copy the git event manager
-#copyfile(srcfile = os.path.join(program_path, git_hook), destfile = os.path.join(target_dir, git_dirs['event_dir'], git_hook))
-
-## Register the git event hooks
-#for event_name in git_hooks:
-#    registergitevent(eventname = event_name['git_event'], eventcode = event_name['event_code'])
 
 
 # Calculate the elapsed times
@@ -261,8 +279,8 @@ if summary_flag:
     sys.stderr.write("\n")
 elif debug_flag:
     sys.stderr.write('************ END ****************\n')
-    sys.stderr.write('Hook path: %s\n' % str(hook_path))
-    sys.stderr.write('Hook name: %s\n' % str(hook_name))
+    sys.stderr.write('Program path: %s\n' % str(program_path))
+    sys.stderr.write('Program executable: %s\n' % str(program_executable))
     sys.stderr.write('*********************************\n')
     sys.stderr.write("\n")
     sys.stderr.write("\n")
