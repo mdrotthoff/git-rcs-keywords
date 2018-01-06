eef2af69267e73087b16e303bfacda8b1f10a3ac
david_rotthoff <david_rotthoff@rotthoff.home>
2018-01-06 14:24:42 -0600

    Modified to support installing to a specified directory

--- install.py
+++ install.py
@@ -13,14 +13,7 @@
 This module installs the RCS keyword functionality into an
 existing git repository.
 
-TODO: When installing in a defined directory from the command line,
-      need to change directory to the repository before performing 
-      the installation.  Otherwise the wrong config file gets updated!
-
-TODO: Solution: 1) Get cwd,
-                2) change to target dir,
-                3) install,
-                4) return to initial working directory
+TODO: Add submodule support to the installation
 
 NOTE: At the current time, installation into submodules it not supported
 """
