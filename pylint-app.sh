374a30322f27109f98ce85046fc576922feaf330
David Rotthoff <drotthoff@gmail.com>
2018-01-10 15:52:40 -0600

    Enhance app to check single file vs all python files

--- pylint-app.sh
+++ pylint-app.sh
@@ -5,7 +5,35 @@
 # $Date$
 # $Source$
 
+generate_lint()
+{
+  local app="${1}"
+  local logfile="${app%.*}.lint"
+  
+  if [[ -z "${1}" ]] ; then
+    echo "No application name provided to generate_lint"
+  else
+    echo "Processing file: ${app}"
+    {
+      echo "Processing file: ${app}"
+      echo " "
+      echo " "
+      echo "pycodestyle:"
+      echo " "
+      pycodestyle -v --show-source --statistics "${app}"
+      echo " "
+      echo " "
+      echo "pylint:"
+      echo " "
+      pylint "${app}"
+    } > "${logfile}" 2>&1
+  fi
+}
 
-for app in *.py ; do
-  pylint "${app}" >> "${app}.lint" 2>&1
-done
+if [[ -z "${1}" ]] ; then
+  for app_file in *.py ; do
+    generate_lint "${app_file}"
+  done
+else
+  generate_lint "${1}"
+fi
