ad7d2cd07c3f1f4151a949490bbab99dddad9cce
David Rotthoff <david.rotthoff@coxautoinc.com>
2021-02-11 10:19:44 -0600

    Modify installer to replace git-hook.py symlinks w/ copy of the manager code

--- src/rcs-filter-smudge.py
+++ src/rcs-filter-smudge.py
@@ -17,7 +17,7 @@ import logging
 __author__ = "David Rotthoff"
 __email__ = "drotthoff@gmail.com"
 __project__ = "git-rcs-keywords"
-__version__ = "1.1.1-dev1-3"
+__version__ = "1.1.1-dev1-4"
 __date__ = "2021-02-07 10:51:24"
 __credits__ = []
 __status__ = "Production"
