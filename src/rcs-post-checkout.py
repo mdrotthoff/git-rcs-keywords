c460455b563ea89f71f5e4e904f71c9036a339c9
David Rotthoff <david.rotthoff@coxautoinc.com>
2021-02-11 14:19:54 -0600

    Release first beta

--- src/rcs-post-checkout.py
+++ src/rcs-post-checkout.py
@@ -22,7 +22,7 @@ import logging
 __author__ = "David Rotthoff"
 __email__ = "drotthoff@gmail.com"
 __project__ = "git-rcs-keywords"
-__version__ = "1.1.1-alpha2-14"
+__version__ = "1.1.1-beta1-14"
 __date__ = "2021-02-07 10:51:24"
 __credits__ = []
 __status__ = "Production"
